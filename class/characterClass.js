b87f4ba735a506fd22bc0e660b21a92339289dda
Aron van Duijn <vanduijnaronzakelijk@gmail.com>
2025-12-06 21:34:24 +0100

    added sword functionality

--- class/characterClass.js
+++ class/characterClass.js
@@ -13,40 +13,62 @@ class Character{
         this.controlType = controlType;
         this.xPosOffset = 0;
         this.canHaveOffset = canHaveOffset;
+        
         this.pressed = {
             "w": false,
             "s": false,
             "up": false,
             "down": false,
+            "d": false,
+            "right": false
         };
+
+        this.canUseEquipement = true;
+        this.equipementCooldown = 800;
+        this.swordAttackDuration = 500;
+        this.equipementWidth = 20;
+        this.equipementHeight = 20;
+        this.lastEquipementUseTime = null;
     }
 
     draw(ctx, canvas, lanes, fighting = false){
+        let currentTime = new Date();
+
+        let lane = lanes[this.pos - 1];
+        
+        //calc this.posY for player so it stands on correct lane on the battleground
+        this.posY = this.calcY(lane, this.height);
+
         if(!(fighting)){
             this.controlCharacter(lanes)
         }
 
-        let lane = lanes[this.pos - 1];
+        if(currentTime - this.lastEquipementUseTime > this.swordAttackDuration){
+            this.swordAttack = false;
+        }
 
-        let position = this.calcPosition(lane);
+        if(currentTime - this.lastEquipementUseTime > this.equipementCooldown){
+            this.canUseEquipement = true;
+        }
+
+        if(this.swordAttack){
+            this.sword(ctx, canvas, lane)
+        }
 
         ctx.fillStyle = this.color;
 
-        ctx.fillRect(position.posX + this.xPosOffset, position.posY, this.width, this.height, this.color);
+        ctx.fillRect(this.posX + this.xPosOffset, this.posY, this.width, this.height, this.color);
     }
 
-    calcPosition(lane){
-        let posY = lane.posY + lane.height / 2 - this.height / 2;
-        let posX = 10;
+    calcY(lane, height){
+        let posY = lane.posY + lane.height / 2 - height / 2;
 
-        this.posX = posX;
-        this.posY = posY;
-
-        return {"posX": posX, "posY": posY};
+        return posY;
     }
 
     controlCharacter(lanes){
         this.switchLane(lanes)
+        this.equipement(lanes);
     }
 
     switchLane(lanes){
@@ -93,6 +115,38 @@ class Character{
         })
     }
 
+    equipement(lane){
+        document.addEventListener("keydown", (event) => {
+            switch(event.key){
+                case "d":
+                    if(!(this.pressed.d) && this.controlType == "wasd" && this.canUseEquipement){
+                        this.swordAttack = true;
+                        this.pressed.d = true;
+                        this.lastEquipementUseTime = new Date();
+                        this.canUseEquipement = false;
+                    }
+                    break;
+                case "ArrowRight":
+                    if(!(this.pressed.right) && this.controlType == "arrows" && this.canUseEquipement){
+                        this.swordAttack = true;
+                        this.pressed.right = true;
+                        this.lastEquipementUseTime = new Date();
+                        this.canUseEquipement = false;
+                    }
+                    break;
+            }
+        });
+
+        document.addEventListener("keyup", (event) => {
+            switch(event.key){
+                case "d":
+                    this.pressed.d = false;
+                case "ArrowRight":
+                    this.pressed.right = false;
+            }
+        });
+    }
+
     checkCollision(characters){
         //check collision with other player
         characters.forEach((character) => {
@@ -106,5 +160,11 @@ class Character{
                 }
             }
         })
+    }
+
+    sword(ctx, canvas, lane){
+        this.equipementPosY = this.calcY(lane, this.equipementHeight);
+
+        ctx.fillRect(this.posX + this.width, this.equipementPosY, this.equipementHeight, this.equipementHeight);
     }
 }
